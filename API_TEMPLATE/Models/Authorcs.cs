a30e68952b2d50b33e23e15459193ac1b38e9929
12201010 - Nguyễn Minh Phát <yu12201010@yersin.edu.vn>
2024-03-29 15:47:42 +0700

    Phat dep chai

---
+++ API_TEMPLATE/Models/Authorcs.cs
@@ -0,0 +1,16 @@
+﻿using API_TEMPLATE.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_TEMPLATE.Models
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        // One-to-many relationship with books
+        public List<Book>? Books { get; set; }
+    }
+}
--- API_TEMPLATE/Program.cs
+++ API_TEMPLATE/Program.cs
@@ -1,3 +1,10 @@
+using System.Text.Json.Serialization;
+using API_TEMPLATE.Data;
+using API_TEMPLATE.Services;
+using Microsoft.EntityFrameworkCore;
+using API_TEMPLATE.Data;
+using API_TEMPLATE.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Library Service to use it with Dependency Injection in Controllers
+builder.Services.AddTransient<ILibraryService, LibraryService>();
+
+// Register database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
